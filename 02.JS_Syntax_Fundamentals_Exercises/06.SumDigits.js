26c3efbfdac444c9496c281c699d9028690ea7b0
peterdidimitrov <petr_dimitrov@abv.bg>
2023-06-24 15:40:38 +0300

    Add different solutions of 06.Sum Digits and 10.Same Numbers from JS Syntax Fundamentals Exercises

--- 02.JS_Syntax_Fundamentals_Exercises/06.SumDigits.js
+++ 02.JS_Syntax_Fundamentals_Exercises/06.SumDigits.js
@@ -1,11 +1,18 @@
 function sum(number) {
-    let stringNumber = String(number);
-    let sum = 0;
-    while (stringNumber !== '') {
-        let digit = Number(stringNumber.charAt(stringNumber.length-1));
-        stringNumber = stringNumber.substring(0, stringNumber.length - 1);
-        sum += digit;
-    }
-    console.log(sum);
+  // let stringNumber = String(number);
+  // let sum = 0;
+  // while (stringNumber !== '') {
+  //     let digit = Number(stringNumber.charAt(stringNumber.length-1));
+  //     stringNumber = stringNumber.substring(0, stringNumber.length - 1);
+  //     sum += digit;
+  // }
+  let sum = 0;
+
+  while (number > 0) {
+    let digit = number % 10;
+    sum += digit;
+    number = Math.floor(number / 10);
+  }
+  console.log(sum);
 }
-sum (543);
+sum(97561);
--- 02.JS_Syntax_Fundamentals_Exercises/10.SameNumbers.js
+++ 02.JS_Syntax_Fundamentals_Exercises/10.SameNumbers.js
@@ -1,31 +1,45 @@
-function solve(number) {
-    let stringNumber = String(number);
-    let sum = 0;
-    let areAllNumbersTheSame = false;
-    
-    if (stringNumber.length === 1) {
-        areAllNumbersTheSame = true;
-    }
-    else {
-        for (let index = 0; index < stringNumber.length - 1; index++) {
-            if (stringNumber[index] === stringNumber[index + 1]) {
-                areAllNumbersTheSame = true;
-            }
-            else {
-                areAllNumbersTheSame = false;
-                break;
-            }
-        }
-    }
+// function solve(number) {
+//     let stringNumber = String(number);
+//     let sum = 0;
+//     let areAllNumbersTheSame = false;
 
-    while (stringNumber !== '') {
-        let digit = Number(stringNumber.charAt(stringNumber.length-1));
-        
-        sum += digit;
-        stringNumber = stringNumber.substring(0, stringNumber.length - 1);
-    }
+//     if (stringNumber.length === 1) {
+//         areAllNumbersTheSame = true;
+//     }
+//     else {
+//         for (let index = 0; index < stringNumber.length - 1; index++) {
+//             if (stringNumber[index] === stringNumber[index + 1]) {
+//                 areAllNumbersTheSame = true;
+//             }
+//             else {
+//                 areAllNumbersTheSame = false;
+//                 break;
+//             }
+//         }
+//     }
 
-    console.log(areAllNumbersTheSame);
-    console.log(sum);
+//     while (stringNumber !== '') {
+//         let digit = Number(stringNumber.charAt(stringNumber.length-1));
+
+//         sum += digit;
+//         stringNumber = stringNumber.substring(0, stringNumber.length - 1);
+//     }
+
+//     console.log(areAllNumbersTheSame);
+//     console.log(sum);
+// }
+// solve(0);
+
+"use strict";
+
+function checkIfSllDigitsAreSame(number) {
+  const digits = Array.from(String(number), Number);
+  const isConsistent = new Set(digits).size === 1;
+  const sum = digits.reduce(function (total, number) {
+    return total + number;
+  }, 0);
+
+  console.log(isConsistent);
+  console.log(sum);
 }
-solve(0);
+checkIfSllDigitsAreSame(2222222);
