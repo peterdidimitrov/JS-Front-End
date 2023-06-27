a2ac6916406e773515cb9bc20af387fda17351e0
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-06-27 17:32:50 +0300

    Change solutions

--- 02.JS_Syntax_Fundamentals_Exercises/15.ListOfNames.js
+++ 02.JS_Syntax_Fundamentals_Exercises/15.ListOfNames.js
@@ -1,5 +1,5 @@
 function sortTheNames(array) {
-    let sortedArray = array.sort();
+    let sortedArray = array.sort((a, b) => a.localeCompare(b));
     for (let index = 0; index < sortedArray.length; index++) {
         console.log(`${index + 1}.${sortedArray[index]}`);
     }
