80dab318828498fdb16e4c93c66c8aa7ef3251f4
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-10 11:10:00 +0300

    Update 04.Movies.js

--- 08.ObjectsAndClasses-Exercises/04.Movies.js
+++ 08.ObjectsAndClasses-Exercises/04.Movies.js
@@ -1,57 +1,35 @@
-function createMovieCatalog(array) {
-  let movies = [];
+function createMovieCatalog(input) {
+  let movieCatalog = [];
 
-  for (const line of array) {
-    if (line.includes("addMovie")) {
-      let name = line.slice(9);
-      addMovie(name);
-    } else if (line.includes("directedBy")) {
-      let movieName = line.split(" directedBy ")[0];
-      let directorName = line.split(" directedBy ")[1];
-      addDirector(movieName, directorName);
-    } else {
-      let movieName = line.split(" onDate ")[0];
-      let date = line.split(" onDate ")[1];
-      addDate(movieName, date);
+  input.forEach((command) => {
+    if (command.includes("addMovie")) {
+      const [_, name] = command.split("addMovie ");
+      movieCatalog.push({
+        name,
+      });
+    } else if (command.includes("directedBy")) {
+      const [movieName, directorName] = command.split(" directedBy ");
+      const movie = movieCatalog.find((m) => m.name === movieName);
+      if (movie) {
+        movie.director = directorName;
+      }
+    } else if (command.includes("onDate")) {
+      const [movieName, date] = command.split(" onDate ");
+      const movie = movieCatalog.find((m) => m.name === movieName);
+      if (movie) {
+        movie.date = date;
+      }
     }
-  }
-
-  let filteredMovies = movies.filter(
-    (movie) =>
-      movie.hasOwnProperty("name") &&
-      movie.hasOwnProperty("director") &&
-      movie.hasOwnProperty("date")
-  );
-
-  for (const movie of filteredMovies) {
-    console.log(JSON.stringify(movie));
-  }
-
-  function addMovie(name) {
-    movies.push({ name });
-  }
-
-  function addDirector(name, director) {
-    let movie = movies.find((m) => m.name === name);
-    if (movie) {
-      movie.director = director;
-    }
-  }
-
-  function addDate(name, date) {
-    let movie = movies.find((m) => m.name === name);
-    if (movie) {
-      movie.date = date;
-    }
-  }
+  });
+  movieCatalog
+    .filter((m) => m.name && m.director && m.date)
+    .forEach((m) => console.log(JSON.stringify(m)));
 }
 createMovieCatalog([
-  "addMovie Fast and Furious",
-  "addMovie Godfather",
-  "Inception directedBy Christopher Nolan",
-  "Godfather directedBy Francis Ford Coppola",
-  "Godfather onDate 29.07.2018",
-  "Fast and Furious onDate 30.07.2018",
-  "Batman onDate 01.08.2018",
-  "Fast and Furious directedBy Rob Cohen",
+  "addMovie The Avengers",
+  "addMovie Superman",
+  "The Avengers directedBy Anthony Russo",
+  "The Avengers onDate 30.07.2010",
+  "Captain America onDate 30.07.2010",
+  "Captain America directedBy Joe Russo",
 ]);
