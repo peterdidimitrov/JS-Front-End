31a2f968643ea27435515c07b76b9fbba35f3c86
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-10 16:57:47 +0300

    Upload solutions from Object and classes - exercises

---
+++ 08.ObjectsAndClasses-Exercises/5.Inventory.js
@@ -0,0 +1,23 @@
+function createHeroCatalog(input) {
+  let heroes = [];
+  input.forEach((element) => {
+    let [name, level, ...items] = element.split(" / ");
+    heroes.push({
+      name,
+      level,
+      items,
+    });
+  });
+  heroes
+    .sort((a, b) => a.level - b.level)
+    .forEach((hero) => {
+      console.log(`Hero: ${hero.name}`);
+      console.log(`level => ${hero.level}`);
+      console.log(`items => ${hero.items}`);
+    });
+}
+createHeroCatalog([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
