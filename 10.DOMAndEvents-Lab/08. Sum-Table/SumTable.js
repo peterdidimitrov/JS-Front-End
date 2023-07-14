729178f3be8df41aef044eace8eb03e7a56aab3e
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-14 08:34:19 +0300

    Update Sum Table

--- 10.DOMAndEvents-Lab/08. Sum-Table/SumTable.js
+++ 10.DOMAndEvents-Lab/08. Sum-Table/SumTable.js
@@ -1,5 +1,7 @@
 function sumTable() {
-  const prices = Array.from(document.querySelectorAll("td:nth-child(2)"));
+  const prices = Array.from(
+    document.querySelectorAll("td:nth-child(2):not(#sum)")
+  );
   const total = prices.reduce((acc, curr) => {
     return acc + Number(curr.textContent);
   }, 0);
