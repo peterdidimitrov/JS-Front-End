6a67968b3cb4b75b63fc49ae0f37112421e38f09
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-14 08:30:48 +0300

    Update app.js

--- 10.DOMAndEvents-Lab/09. Highlight-Active/app.js
+++ 10.DOMAndEvents-Lab/09. Highlight-Active/app.js
@@ -2,10 +2,10 @@ function focused() {
   const inputs = Array.from(document.querySelectorAll("input"));
   inputs.forEach((input) => {
     input.addEventListener("focus", (e) => {
-      e.target.parentElement.className = "focused";
+      e.target.parentElement.classList.add("focused");
     });
     input.addEventListener("blur", (e) => {
-      e.target.parentElement.className = "";
+      e.target.parentElement.classList.remove("focused");
     });
   });
 }
