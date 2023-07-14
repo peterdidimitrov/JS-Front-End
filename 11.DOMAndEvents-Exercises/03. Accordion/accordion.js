619623ca71acceec6707b60c6119b30bf3562437
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-14 16:54:40 +0300

    Update 03.Accordion

--- 11.DOMAndEvents-Exercises/03. Accordion/accordion.js
+++ 11.DOMAndEvents-Exercises/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+  const button = document.querySelector(".button");
+  const aditionalText = document.querySelector("#extra");
+
+  if (aditionalText.style.display === "none") {
+    aditionalText.style.display = "block";
+    button.textContent = "Less";
+  } else {
+    aditionalText.style.display = "none";
+    button.textContent = "More";
+  }
 }
