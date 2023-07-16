af51e10fead62451f7f8151828d78ed04a694338
peterdidimitrov <petr_dimitrov@abv.bg>
2023-07-16 20:12:22 +0300

    Update 04. Locked profile

--- 11.DOMAndEvents-Exercises/04. Locked-Profile/app.js
+++ 11.DOMAndEvents-Exercises/04. Locked-Profile/app.js
@@ -1,20 +1,21 @@
 function lockedProfile() {
   const buttons = Array.from(document.querySelectorAll("button"));
-  const hidenInfo = Array.from(document.querySelectorAll("#user1HiddenFields"));
 
   buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const lockedRadioButton = e.currentTarget.parentElement.querySelector(
-        'input[type="radio"]'
-      );
-      if (lockedRadioButton.checked) {
-        return;
-      }
-      const hiddenInfo = e.target.parentElement.querySelector("div");
-      hiddenInfo.style.display =
-        hiddenInfo.style.display === "block" ? "none" : "block";
-      e.currentTarget.textContent =
-        e.currentTarget.textContent === "Show more" ? "Hide it" : "Show more";
-    });
+    button.addEventListener("click", hendleClick);
   });
+  
+  function hendleClick(e) {
+    const lockedRadioButton = e.currentTarget.parentElement.querySelector(
+      'input[type="radio"]'
+    );
+    if (lockedRadioButton.checked) {
+      return;
+    }
+    const hiddenInfo = e.target.parentElement.querySelector("div");
+    hiddenInfo.style.display =
+      hiddenInfo.style.display === "block" ? "none" : "block";
+    e.currentTarget.textContent =
+      e.currentTarget.textContent === "Show more" ? "Hide it" : "Show more";
+  }
 }
