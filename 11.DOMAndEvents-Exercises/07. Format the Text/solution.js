73a426e06e9c107b1816038b194c5cf2f06bb690
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-17 12:14:23 +0300

    Upload Format the Text solution

--- 11.DOMAndEvents-Exercises/07. Format the Text/solution.js
+++ 11.DOMAndEvents-Exercises/07. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  const sentences = document.querySelector("#input").value.split(".");
+  sentences.pop();
+
+  console.log(sentences);
+  const output = document.querySelector("#output");
+
+  while (sentences.length > 0) {
+    const p = document.createElement("p");
+    output.appendChild(p);
+    p.textContent = `${sentences.splice(0, 3).join(".")}.`;
+  }
 }
