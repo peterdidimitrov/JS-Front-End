1338433a3200ade4249d8db381d08572da52593f
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-17 08:32:03 +0300

    Update Fill Dropdown solution

--- 11.DOMAndEvents-Exercises/05. Fill-Dropdown/dropdown.js
+++ 11.DOMAndEvents-Exercises/05. Fill-Dropdown/dropdown.js
@@ -2,13 +2,22 @@ function addItem() {
   const textInput = document.querySelector("#newItemText");
   const valueInput = document.querySelector("#newItemValue");
 
-  const option = document.createElement("option");
-  option.textContent = textInput.value;
-  option.setAttribute("value", valueInput.value);
+  const option = createOption(textInput.value, valueInput.value);
 
   const select = document.querySelector("#menu");
   select.appendChild(option);
 
-  textInput.value = "";
-  valueInput.value = "";
+  clearInputFields(textInput, valueInput);
+}
+function createOption(text, value) {
+  const option = document.createElement("option");
+  option.textContent = text;
+  option.value = value;
+
+  return option;
+}
+function clearInputFields(...inputs) {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
 }
