18c1e939bd3e5161f681b7314f965e5b967a45b1
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-17 09:41:43 +0300

    Upload 05.Table Search Engine solution

--- 11.DOMAndEvents-Exercises/06. Table - Search Engine/solution.js
+++ 11.DOMAndEvents-Exercises/06. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const tableFields = Array.from(
+      document.querySelectorAll("tbody > tr > td")
+    );
+    const serchedText = document
+      .querySelector("#searchField")
+      .value.toLowerCase();
+    const activeRows = Array.from(document.querySelectorAll("tbody tr.select"));
 
-   }
+    activeRows.forEach((row) => {
+      row.classList.remove("select");
+    });
+
+    tableFields.forEach((field) => {
+      const row = field.parentElement;
+      if (field.textContent.toLowerCase().includes(serchedText)) {
+        row.classList.add("select");
+      }
+    });
+    document.querySelector("input").value = "";
+  }
 }
