781038064f119842334ee8a6c2f62c36a0aae7c7
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-19 09:12:33 +0300

    Upload solutions from HTTP and REST

---
+++ 12.HTTP-and-REST-Lab/04. GitHub-Repos/scripts/app.js
@@ -0,0 +1,7 @@
+async function loadRepos() {
+  let response = await fetch(
+    "https://api.github.com/users/peterdidimitrov/repos"
+  );
+  let data = await response.json();
+  document.querySelector("#res").textContent = JSON.stringify(data);
+}
---
+++ 12.HTTP-and-REST-Lab/06. Github-Commits/app.js
@@ -0,0 +1,23 @@
+async function loadCommits() {
+  const username = document.querySelector("#username").value;
+  const repo = document.querySelector("#repo").value;
+  const list = document.querySelector("#commits");
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${username}/${repo}/commits`
+    );
+    if (response.ok === false) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    list.innerHTML = "";
+
+    data.forEach(({ commit }) => {
+      list.innerHTML += `<li>
+      ${commit.author.name}: ${commit.message}
+		  </li>`;
+    });
+  } catch (err) {
+    list.innerHTML = `Error: ${err.status} (Not Found)`;
+  }
+}
