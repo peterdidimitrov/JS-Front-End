24c0319c7c0cacd37e5763fdbc42e58df0a6a69b
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-07-27 09:35:36 +0300

    Update 01.Sprint Review solution

--- 14.Exam preparation/03.JSFront-EndRegularExam-8Apr2023/01.SprintReview/01.SprintReview.js
+++ 14.Exam preparation/03.JSFront-EndRegularExam-8Apr2023/01.SprintReview/01.SprintReview.js
@@ -42,7 +42,7 @@ function solve(array) {
           taskId,
           title,
           status,
-          points: Number(estimatePoints),
+          estimatePoints: Number(estimatePoints),
         });
       }
     } else if (command === "Change Status") {
@@ -66,7 +66,7 @@ function solve(array) {
         console.log(`Assignee ${currentAssignee} does not exist on the board!`);
       } else {
         let [index] = commandInfo;
-        if (index < 0 || index > assignees[currentAssignee].length) {
+        if (index < 0 || index > assignees[currentAssignee].length - 1) {
           console.log(`Index is out of range!`);
         } else {
           assignees[currentAssignee].splice(index, 1);
@@ -74,25 +74,40 @@ function solve(array) {
       }
     }
   });
+  const toDoPoints = sumPoints("ToDo");
+  const inProgresPoints = sumPoints("In Progress");
+  const codeReviewPoints = sumPoints("Code Review");
+  const donePoints = sumPoints("Done");
 
-  Object.values(assignees)
-    .flat()
-    .filter((t) => t.status === assignees.status)
-    .reduce((acc, curr) => {
-      return acc + curr.points;
-    }, 0);
+  console.log(`ToDo: ${toDoPoints}pts`);
+  console.log(`In Progress: ${inProgresPoints}pts`);
+  console.log(`Code Review: ${codeReviewPoints}pts`);
+  console.log(`Done Points: ${donePoints}pts`);
 
-  console.log(JSON.stringify(assignees, null, 2));
+  function sumPoints(status) {
+    let points = Object.values(assignees)
+      .flat()
+      .filter((t) => t.status === status)
+      .reduce((acc, curr) => {
+        return (acc += curr.estimatePoints);
+      }, 0);
+    return points;
+  }
+  if (donePoints >= toDoPoints + inProgresPoints + codeReviewPoints) {
+    console.log("Sprint was successful!");
+  } else {
+    console.log("Sprint was unsuccessful...");
+  }
 }
 solve([
-  "5",
-  "Kiril:BOP-1209:Fix Minor Bug:ToDo:3",
-  "Mariya:BOP-1210:Fix Major Bug:In Progress:3",
-  "Peter:BOP-1211:POC:Code Review:5",
-  "Georgi:BOP-1212:Investigation Task:Done:2",
-  "Mariya:BOP-1213:New Account Page:In Progress:13",
-  "Add New:Kiril:BOP-1217:Add Info Page:In Progress:5",
-  "Change Status:Peter:BOP-1290:ToDo",
-  "Remove Task:Mariya:1",
-  "Remove Task:Joro:1",
+  "4",
+  "Kiril:BOP-1213:Fix Typo:Done:1",
+  "Peter:BOP-1214:New Products Page:In Progress:2",
+  "Mariya:BOP-1215:Setup Routing:ToDo:8",
+  "Georgi:BOP-1216:Add Business Card:Code Review:3",
+  "Add New:Sam:BOP-1237:Testing Home Page:Done:3",
+  "Change Status:Georgi:BOP-1216:Done",
+  "Change Status:Will:BOP-1212:In Progress",
+  "Remove Task:Georgi:3",
+  "Change Status:Mariya:BOP-1215:Done",
 ]);
