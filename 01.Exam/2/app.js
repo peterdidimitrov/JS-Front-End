f0dcbbcfbd3681c9172cc9ab99e688008a98407e
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-08-29 11:37:08 +0300

    Update solution

--- 01.Exam/2/app.js
+++ 01.Exam/2/app.js
@@ -1,15 +1,11 @@
 window.addEventListener("load", solve);
 
 function solve() {
+  let formElement = document.querySelector("form");
   let studentName = document.querySelector("#student");
   let university = document.querySelector("#university");
   let score = document.querySelector("#score");
-
   let nextButton = document.querySelector("#next-btn");
-
-  let ulPreviewList = document.querySelector("#preview-list");
-  let ulCandidatesList = document.querySelector("#candidates-list");
-
   nextButton.addEventListener("click", moveToPreview);
 
   let nameInfo = "";
@@ -25,48 +21,42 @@ function solve() {
       return;
     }
 
+    let ulPreviewList = document.querySelector("#preview-list");
+    let ulCandidatesList = document.querySelector("#candidates-list");
     let liElement = createElement("li", "", ulPreviewList);
     liElement.className = "application";
     let article = createElement("article", "", liElement);
-    let h4 = createElement("h4", studentName.value, article);
-    let pUniversity = createElement(
-      "p",
-      `University: ${university.value}`,
-      article
-    );
-    let pScore = createElement("p", `Score: ${Number(score.value)}`, article);
-
-    let editButton = createElement("buttom", "edit", liElement);
-    editButton.className = "action-btn edit";
-    let applyButton = createElement("buttom", "apply", liElement);
-    applyButton.className = "action-btn apply";
-
+    createElement("h4", studentName.value, article);
+    createElement("p", `University:${" "}${university.value}`, article);
+    createElement("p", `Score:${" "}${score.value}`, article);
+    let editButton = createElement("button", "edit", liElement);
+    editButton.classList.add("action-btn");
+    editButton.classList.add("edit");
+    let applyButton = createElement("button", "apply", liElement);
+    applyButton.classList.add("action-btn");
+    applyButton.classList.add("apply");
     editButton.addEventListener("click", editInfo);
     applyButton.addEventListener("click", applyInfo);
-
-    nextButton.disabled = true;
-
     nameInfo = studentName.value;
     universityInfo = university.value;
     scoreInfo = score.value;
+    formElement.reset();
+    nextButton.disabled = true;
 
-    studentName.value = "";
-    university.value = "";
-    score.value = "";
     function editInfo() {
-      ulPreviewList.innerHTML = "";
       studentName.value = nameInfo;
       university.value = universityInfo;
       score.value = scoreInfo;
-
+      ulPreviewList.removeChild(liElement);
       nextButton.disabled = false;
     }
-    function applyInfo(e) {
+
+    function applyInfo() {
+      ulPreviewList.removeChild(liElement);
+      liElement.removeChild(applyButton);
+      liElement.removeChild(editButton);
+      ulCandidatesList.appendChild(liElement);
       nextButton.disabled = false;
-      editButton.remove();
-      e.target.remove();
-      ulCandidatesList.innerHTML = ulPreviewList.innerHTML;
-      ulPreviewList.innerHTML = "";
     }
 
     function createElement(elementTag, value, parent) {
@@ -75,7 +65,6 @@ function solve() {
       if (parent) {
         parent.appendChild(newElement);
       }
-
       return newElement;
     }
   }
